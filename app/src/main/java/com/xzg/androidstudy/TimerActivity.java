8e143916f56cab5f4769ab0889c21bfa0ec80d4e
xzg <xzg248369@163.com>
2021-10-15 16:00:38 +0800

    add timer

--- app/src/main/java/com/xzg/androidstudy/TimerActivity.java
+++ app/src/main/java/com/xzg/androidstudy/TimerActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,7 +17,12 @@ import android.widget.TextView;
 public class TimerActivity extends AppCompatActivity {
 
     private TextView timerTitle, timerNum, timerTxt;
-    private ImageView playerBtn;
+    private ImageView playerBtn, playerBtn2;
+    private boolean flag = false;
+
+
+    // post postDelay postAtTime
+    private Handler timerHandler2 = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +33,25 @@ public class TimerActivity extends AppCompatActivity {
         timerNum = findViewById(R.id.timer_num);
         timerTxt = findViewById(R.id.timer_txt);
         playerBtn = findViewById(R.id.timer_btn);
+        playerBtn2 = findViewById(R.id.timer_btn2);
 
         playerBtn.setOnClickListener(new ClickHandler());
+        playerBtn2.setOnClickListener(new ClickHandler());
     }
+
+    private int i;
+    private String time;
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            int min = i / 60;
+            int sec = i % 60;
+            time = (min < 10 ? "0" + min : "" + min) + ":" + (sec < 10 ? "0" + sec : "" + sec);
+            timerNum.setText(time);
+            i++;
+            timerHandler2.postDelayed(runnable, 1000);
+        }
+    };
 
     // 创建点击事件类
     class ClickHandler implements View.OnClickListener {
@@ -37,24 +59,58 @@ public class TimerActivity extends AppCompatActivity {
         public void onClick(View v) {
             int id = v.getId();
             if (id == R.id.timer_btn) {
-                timerTitle.setText("工作中...");
-                playerBtn.setImageResource(R.mipmap.player_stop);
-
-                new Thread() {
-                    @Override
-                    public void run() {
-                        super.run();
-                        int i = 1;
-                        while (true) {
-                            try {
-                                sleep(1000);
-                            } catch (Exception e) {
-                                e.printStackTrace();
+                if (!flag) {
+                    flag = true;
+                    timerTitle.setText("工作中...");
+                    playerBtn.setImageResource(R.mipmap.player_stop);
+                    timerTxt.setText("");
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            int i = 1;
+                            Log.d("---线程内flag1", flag + "");
+                            while (flag) {
+                                Log.d("---线程内flag2", flag + "");
+                                try {
+                                    sleep(1000);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                Message msg = new Message();
+                                msg.arg1 = i;
+                                timerHandler.sendMessage(msg);
+                                // 时间 ++
+                                i++;
                             }
-                            i++;
                         }
-                    }
-                }.start();
+                    }.start();
+                } else {
+                    flag = false;
+                }
+            } else if (id == R.id.timer_btn2) {
+                // 在post方法中，我们可以处理一切和视图相关的操作
+                if (!flag) {
+                    flag = true;
+                    timerTitle.setText("工作中...");
+                    playerBtn2.setImageResource(R.mipmap.player_stop);
+                    timerTxt.setText("");
+                    i = 1;
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            // 延迟执行
+                            timerHandler2.postDelayed(runnable, 1000);
+                        }
+                    }.start();
+                } else {
+                    flag = false;
+                    timerTitle.setText("计时器");
+                    playerBtn2.setImageResource(R.mipmap.player_start);
+                    timerTxt.setText("用时：" + time);
+                    timerHandler2.removeCallbacks(runnable);
+                }
             }
         }
     }
@@ -63,6 +119,16 @@ public class TimerActivity extends AppCompatActivity {
     final Handler timerHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(@NonNull Message msg) {
+            int min = msg.arg1 / 60;
+            int sec = msg.arg1 % 60;
+            String time = (min < 10 ? "0" + min : "" + min) + ":" + (sec < 10 ? "0" + sec : "" + sec);
+            timerNum.setText(time);
+            Log.d("---handler中的flag", flag + "");
+            if (!flag) {
+                timerTitle.setText("计时器");
+                playerBtn.setImageResource(R.mipmap.player_start);
+                timerTxt.setText("用时：" + time);
+            }
             return false;
         }
     });
