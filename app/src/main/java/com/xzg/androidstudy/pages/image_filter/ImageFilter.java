d7172a4eded3d7426f493d9cc8aa59aed161e1ea
xzg <xzg248369@163.com>
2021-10-28 18:03:02 +0800

    select img and show

--- app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
+++ app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
@@ -6,14 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
 import com.xzg.androidstudy.R;
 import com.xzg.androidstudy.utils.GlideEngine;
+
+import java.io.File;
+import java.util.List;
+
+import jp.co.cyberagent.android.gpuimage.GPUImageView;
 
 public class ImageFilter extends AppCompatActivity {
 
@@ -46,4 +53,23 @@ public class ImageFilter extends AppCompatActivity {
                 .forResult(PictureConfig.CHOOSE_REQUEST);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PictureConfig.CHOOSE_REQUEST) {
+            List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+            Log.d("-------------selectList", String.valueOf(selectList));
+            showSelectPic(selectList);
+        }
+    }
+
+
+    // 展示图片
+    private void showSelectPic(List<LocalMedia> result) {
+        GPUImageView view = findViewById(R.id.img_container);
+        for (LocalMedia l:result){
+            String path = l.getRealPath();
+            view.setImage(new File(path));
+        }
+    }
 }
