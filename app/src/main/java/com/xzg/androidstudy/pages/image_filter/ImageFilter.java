2a3407d4e9b9b8d8d3d16fb0dfd702a9487b3eea
xzg <xzg248369@163.com>
2021-10-28 18:25:01 +0800

    clear cache

--- app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
+++ app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
@@ -14,6 +14,7 @@ import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.tools.PictureFileUtils;
 import com.xzg.androidstudy.R;
 import com.xzg.androidstudy.utils.GlideEngine;
 
@@ -60,6 +61,9 @@ public class ImageFilter extends AppCompatActivity {
             List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
             Log.d("-------------selectList", String.valueOf(selectList));
             showSelectPic(selectList);
+
+            // 清理缓存
+            PictureFileUtils.deleteCacheDirFile(ImageFilter.this,PictureMimeType.ofImage());
         }
     }
 
