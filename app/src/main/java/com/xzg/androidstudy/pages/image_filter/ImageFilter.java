249db621917be2532c5687829af9d1e6e4865283
xzg <xzg248369@163.com>
2021-10-29 12:06:13 +0800

    use seekbar set GPUImage

--- app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
+++ app/src/main/java/com/xzg/androidstudy/pages/image_filter/ImageFilter.java
@@ -4,11 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
@@ -21,27 +23,62 @@ import com.xzg.androidstudy.utils.GlideEngine;
 import java.io.File;
 import java.util.List;
 
+import jp.co.cyberagent.android.gpuimage.GPUImage;
 import jp.co.cyberagent.android.gpuimage.GPUImageView;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageGrayscaleFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSharpenFilter;
 
 public class ImageFilter extends AppCompatActivity {
+
+
+    private GPUImage gpuImage;
+    private SeekBar seekBar;
+    private GPUImageView gpuImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_filter);
-
+        gpuImageView = findViewById(R.id.img_container);
+        // 获取进度条
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
         Button imgSelectBtn = findViewById(R.id.add_log);
+        // 设置灰色滤镜按钮
+        Button grayBtn = findViewById(R.id.set_gray);
         imgSelectBtn.setOnClickListener(new ClickHandler());
+        grayBtn.setOnClickListener(new ClickHandler());
 
+        // 通过进度条设置滤镜
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setGrayFilter(gpuImageView,progress);
+                gpuImageView.requestRender();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     // 添加点击事件
     public class ClickHandler implements View.OnClickListener {
+        GPUImageView view = findViewById(R.id.img_container);
+
         @Override
         public void onClick(View v) {
             int id = v.getId();
             if (id == R.id.add_log) {
                 pictureSelect();
+            } else if (id == R.id.set_gray) {
+                setGrayFilter(view,0);
             }
         }
     }
@@ -63,7 +100,7 @@ public class ImageFilter extends AppCompatActivity {
             showSelectPic(selectList);
 
             // 清理缓存
-            PictureFileUtils.deleteCacheDirFile(ImageFilter.this,PictureMimeType.ofImage());
+            PictureFileUtils.deleteCacheDirFile(ImageFilter.this, PictureMimeType.ofImage());
         }
     }
 
@@ -71,9 +108,16 @@ public class ImageFilter extends AppCompatActivity {
     // 展示图片
     private void showSelectPic(List<LocalMedia> result) {
         GPUImageView view = findViewById(R.id.img_container);
-        for (LocalMedia l:result){
+        for (LocalMedia l : result) {
             String path = l.getRealPath();
             view.setImage(new File(path));
         }
+    }
+
+    // 设置灰色滤镜
+    private void setGrayFilter(GPUImageView view,int percent) {
+        GPUImageSharpenFilter gpuImageSharpenFilter = new GPUImageSharpenFilter();
+        gpuImageSharpenFilter.setSharpness(percent);
+        view.setFilter(gpuImageSharpenFilter);
     }
 }
---
+++ app/src/main/java/com/xzg/androidstudy/utils/GPUImageFilterTools.java
@@ -0,0 +1,9 @@
+package com.xzg.androidstudy.utils;
+
+import android.content.Context;
+
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter;
+
+public class GPUImageFilterTools {
+
+}
