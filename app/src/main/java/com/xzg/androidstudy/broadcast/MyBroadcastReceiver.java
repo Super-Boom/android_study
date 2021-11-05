703c6722e39d2deced164cd73db02ad3fdb87863
xzg <xzg248369@163.com>
2021-11-05 16:39:12 +0800

    add custom boardcast

--- app/src/main/java/com/xzg/androidstudy/broadcast/MyBroadcastReceiver.java
+++ app/src/main/java/com/xzg/androidstudy/broadcast/MyBroadcastReceiver.java
@@ -4,16 +4,38 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.xzg.androidstudy.pages.broadcast.BroadcastActivity;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "BroadcastReceiver";
+
+    private TextView mTextView;
+
+    public MyBroadcastReceiver() {
+
+    }
+
+    public MyBroadcastReceiver(TextView textView) {
+        Log.d("-----广播接收器运行", "广播接收器运行");
+        mTextView = textView;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null) {
             // 接收到的是什么广播
             String action = intent.getAction();
             Log.d(TAG, "-----onReceive:" + action);
+            if (TextUtils.equals(action, BroadcastActivity.MY_ACTION)) {
+                String content = intent.getStringExtra(BroadcastActivity.BROADCAST_CONTENT);
+                if (mTextView != null) {
+                    mTextView.setText(content);
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/xzg/androidstudy/pages/broadcast/BroadcastActivity.java
+++ app/src/main/java/com/xzg/androidstudy/pages/broadcast/BroadcastActivity.java
@@ -8,33 +8,66 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.xzg.androidstudy.R;
 import com.xzg.androidstudy.broadcast.MyBroadcastReceiver;
 
 public class BroadcastActivity extends AppCompatActivity {
-    private MyBroadcastReceiver br;
+    private MyBroadcastReceiver myBroadcastReceiver;
+    public static String MY_ACTION = "XZG";
+    public static String BROADCAST_CONTENT = "broadcast_content";
+
+    private EditText editText;
+    private Button sendBroadcastMsgBtn;
+    private TextView receiveTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast);
+        // 自定义广播
+        editText = findViewById(R.id.broadcast_send_msg);
+        sendBroadcastMsgBtn = findViewById(R.id.sendBroadcastBtn);
+        receiveTextView = findViewById(R.id.broadcast_receive_msg);
+
+        // 注册广播接收器
+        myBroadcastReceiver = new MyBroadcastReceiver(receiveTextView);
+        // 为广播接收器添加action
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+//        filter.addDataScheme("package");
+        filter.addAction(MY_ACTION);
 
 
         // 注册广播接收器
-        br = new MyBroadcastReceiver();
-        IntentFilter filter = new IntentFilter(ConnectivityManager.EXTRA_CAPTIVE_PORTAL);
-        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        filter.addDataScheme("package");
-        this.registerReceiver(br, filter);
+        registerReceiver(myBroadcastReceiver, filter);
+
+        // 发送广播
+        sendBroadcastMsgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 新建广播
+                Intent intent = new Intent();
+                intent.setAction(MY_ACTION);
+                // 放入广播要携带的数据
+                intent.putExtra(BROADCAST_CONTENT, editText.getText().toString());
+                sendBroadcast(intent);
+                Log.d("-----","hahah");
+            }
+        });
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // 取消广播接收器
-        if (br != null) {
-            unregisterReceiver(br);
+        if (myBroadcastReceiver != null) {
+            unregisterReceiver(myBroadcastReceiver);
         }
     }
 
