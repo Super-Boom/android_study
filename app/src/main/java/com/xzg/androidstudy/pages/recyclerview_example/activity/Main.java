86ffd5eef6bc8829900bc69cde86615124f53fdb
xzg <xzg248369@163.com>
2021-11-16 18:50:46 +0800

    android waterfall

--- app/src/main/java/com/xzg/androidstudy/pages/recyclerview_example/activity/Main.java
+++ app/src/main/java/com/xzg/androidstudy/pages/recyclerview_example/activity/Main.java
@@ -1,5 +1,6 @@
 package com.xzg.androidstudy.pages.recyclerview_example.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -19,6 +21,8 @@ import java.util.List;
 public class Main extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerViewAdapter recyclerViewAdapter;
+    private LinearLayoutManager linearLayoutManager;
+    private StaggeredGridLayoutManager staggeredGridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,16 +66,31 @@ public class Main extends AppCompatActivity {
                     recyclerView.setLayoutManager(gridLayoutManager);
                 } else if (recyclerView.getLayoutManager().getClass() == GridLayoutManager.class) {
                     // 瀑布流布局
-                    StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+                    staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+                    // 解决滑动位置变化
+                    staggeredGridLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
                     recyclerView.setLayoutManager(staggeredGridLayoutManager);
                 } else {
                     // 线性布局
-                    LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(v.getContext());
-                    recyclerView.setLayoutManager(linearLayoutManager1);
+                    recyclerView.setLayoutManager(linearLayoutManager);
+
                 }
             }
         });
-    }
 
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                staggeredGridLayoutManager.invalidateSpanAssignments();
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
 
 }
